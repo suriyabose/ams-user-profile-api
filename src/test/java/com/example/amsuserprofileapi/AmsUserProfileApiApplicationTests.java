d2abd466199ca87d939c2cdbecbc53d0df28df45
suriyabose <suriyakumarbose@gmail.com>
2022-06-01 16:27:20 +0530

    init_commit

---
+++ src/test/java/com/example/amsuserprofileapi/AmsUserProfileApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.amsuserprofileapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmsUserProfileApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
